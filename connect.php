5a519f16e16cfa370b292339c6359f2de7481177
Leah Greenstein <lrg79@cornell.edu>
2016-07-07 15:03:50 -0400

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "localhost";
+$dbusername = "root";
+$dbpassword = "GenjiHanzo9199";
+$dbname = "forms1";
+
+$input1 = $_POST['inputTest'];
+
+$conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
+
+if ($conn -> connect_error){
+	die("Connection failed: " . $conn ->connect_error);
+}
+
+if (empty($input1)){
+	echo "input 1 cannot be blank. Please press back and correct the issue!";
+	die();
+}
+
+$sql = "INSERT INTO demo (input1) VALUES ('$input1')";
+
+if ($conn ->query($sql) === TRUE){
+	echo "Thank you, has been added";
+} else {
+	echo "Error" . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+?>
